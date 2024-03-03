a48aaa50e9bb08ffca5acf19293898fc34a7246c
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-03 22:32:26 +0600

    last section of this website

--- src/pages/Home/BuildTeam.jsx
+++ src/pages/Home/BuildTeam.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import image from "../../images/div (1).png";
 import { FaCheck } from "react-icons/fa6";
 import LearnMoreBtn from "../../components/LearnMoreBtn";
+import icon1 from "../../images/n.png";
+import icon2 from "../../images/figma.png";
+import icon3 from "../../images/diamond.png";
 
 const BuildTeam = () => {
   const [categories2, setCategories2] = useState([]);
@@ -36,17 +39,32 @@ const BuildTeam = () => {
             </div>
           ))}
         </div>
-        <div className="flex items-start mt-5">
+        <div className="flex items-start justify-between mt-5">
           <div className="space-y-4">
-            <h3 className="font-normal text-lg flex gap-1 items-center"><FaCheck /> Build low-fi wireframes</h3>
-            <h3 className="font-normal text-lg flex gap-1 items-center"><FaCheck />
+            <h3 className="font-normal text-lg flex gap-1 items-center">
+              <FaCheck /> Build low-fi wireframes
+            </h3>
+            <h3 className="font-normal text-lg flex gap-1 items-center">
+              <FaCheck />
               Involve stakeholders in the design process
             </h3>
-            <h3 className="font-normal text-lg flex gap-1 items-center"><FaCheck />
+            <h3 className="font-normal text-lg flex gap-1 items-center">
+              <FaCheck />
               Run engaging design workshops
             </h3>
             <LearnMoreBtn></LearnMoreBtn>
+            <div className="mt-7 space-y-3">
+              <h2 className="font-normal text-lg">
+                Integrate your favorite tools
+              </h2>
+              <div className="flex gap-5">
+                <img src={icon3} alt="icon image" />
+                <img src={icon2} alt="icon image" />
+                <img src={icon1} alt="icon image" />
+              </div>
+            </div>
           </div>
+
           <div>
             <img src={image} alt="div image" />
           </div>

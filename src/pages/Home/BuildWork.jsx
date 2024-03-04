6d8eb0e789142ae4ace10b3a48fd151e79e7a9da
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-05 01:18:33 +0600

    work done

--- src/pages/Home/BuildWork.jsx
+++ src/pages/Home/BuildWork.jsx
@@ -25,18 +25,130 @@ const BuildWork = () => {
       <h1 className="text-5xl font-bold text-left">
         Built for the way you work
       </h1>
-      <Tabs>
-        <div className="flex flex-wrap gap-2 items-center  mt-3">
+      <Tabs defaultIndex={1}>
+        <TabList className="flex flex-wrap gap-2 items-center mt-3">
           {categories1?.map((category, index) => (
-            <TabList key={index}>
-              <div className="space-y-4 ">
-                <Tab className="font-normal text-sm border rounded-3xl px-3 py-2 ">
-                  {category.name}
-                </Tab>
-              </div>
-            </TabList>
+            <Tab
+              key={index}
+              className="space-y-4 font-normal text-sm border rounded-3xl px-3 py-2 "
+            >
+              {category.name}
+            </Tab>
           ))}
-        </div>
+        </TabList>
+
+        <TabPanel>
+          <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
+            <div className="space-y-4">
+              <h3 className="font-normal text-lg">Brainstorming</h3>
+              <p className="font-normal text-lg w-1/2">
+                Unleash creative ideas and build on them with the help of sticky
+                notes, images, mind maps, videos, drawing capabilities — the
+                list goes on.
+              </p>
+              <LearnMoreBtn></LearnMoreBtn>
+            </div>
+            <div className="">
+              <img src={image} alt="div image" />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
+            <div className="space-y-4">
+              <h3 className="font-normal text-lg">Diagramming</h3>
+              <p className="font-normal text-lg w-1/2">
+                Unleash creative ideas and build on them with the help of sticky
+                notes, images, mind maps, videos, drawing capabilities — the
+                list goes on.
+              </p>
+              <LearnMoreBtn></LearnMoreBtn>
+            </div>
+            <div className="">
+              <img src={image} alt="div image" />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
+            <div className="space-y-4">
+              <h3 className="font-normal text-lg">Meetings & Workshops</h3>
+              <p className="font-normal text-lg w-1/2">
+                Unleash creative ideas and build on them with the help of sticky
+                notes, images, mind maps, videos, drawing capabilities — the
+                list goes on.
+              </p>
+              <LearnMoreBtn></LearnMoreBtn>
+            </div>
+            <div className="">
+              <img src={image} alt="div image" />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
+            <div className="space-y-4">
+              <h3 className="font-normal text-lg">Scrum Events</h3>
+              <p className="font-normal text-lg w-1/2">
+                Unleash creative ideas and build on them with the help of sticky
+                notes, images, mind maps, videos, drawing capabilities — the
+                list goes on.
+              </p>
+              <LearnMoreBtn></LearnMoreBtn>
+            </div>
+            <div className="">
+              <img src={image} alt="div image" />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
+            <div className="space-y-4">
+              <h3 className="font-normal text-lg">Mapping</h3>
+              <p className="font-normal text-lg w-1/2">
+                Unleash creative ideas and build on them with the help of sticky
+                notes, images, mind maps, videos, drawing capabilities — the
+                list goes on.
+              </p>
+              <LearnMoreBtn></LearnMoreBtn>
+            </div>
+            <div className="">
+              <img src={image} alt="div image" />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
+            <div className="space-y-4">
+              <h3 className="font-normal text-lg">Research & Design</h3>
+              <p className="font-normal text-lg w-1/2">
+                Unleash creative ideas and build on them with the help of sticky
+                notes, images, mind maps, videos, drawing capabilities — the
+                list goes on.
+              </p>
+              <LearnMoreBtn></LearnMoreBtn>
+            </div>
+            <div className="">
+              <img src={image} alt="div image" />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
+            <div className="space-y-4">
+              <h3 className="font-normal text-lg">Strategic Planning</h3>
+              <p className="font-normal text-lg w-1/2">
+                Unleash creative ideas and build on them with the help of sticky
+                notes, images, mind maps, videos, drawing capabilities — the
+                list goes on.
+              </p>
+              <LearnMoreBtn></LearnMoreBtn>
+            </div>
+            <div className="">
+              <img src={image} alt="div image" />
+            </div>
+          </div>
+        </TabPanel>
         <TabPanel>
           <div className="flex flex-col lg:flex-row gap-5 items-start mt-10">
             <div className="space-y-4">
--- src/pages/Home/CollaborateSection.jsx
+++ src/pages/Home/CollaborateSection.jsx
@@ -3,29 +3,53 @@ import { FaArrowRight } from "react-icons/fa6";
 const CollaborateSection = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-center text-5xl text-primary font-bold">Collaborate without <br />constraints</h1>
+      <span className="uppercase bg-[#FFD02F] font-bold px-4 py-2 rounded-3xl rotate-2">
+        Your Idea Starts Here
+      </span>
+      <div className="">
+        <h1 className="text-center text-5xl text-primary font-bold">
+          Collaborate without <br />
+          constraints
+        </h1>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10">
         <div className=" px-12 text-left py-5">
-            <h1 className="font-bold text-primary text-2xl">Free forever</h1>
-            <p className="text-base mt-5">Our free plan gives you unlimited team members, 3 boards, and 300+ export-made templates. Signing up with your work email lets you bring in your team faster. See our <br /> <span className="text-[#4262FF]">pricing plans</span> for more features.</p>
+          <h1 className="font-bold text-primary text-2xl">Free forever</h1>
+          <p className="text-base mt-5">
+            Our free plan gives you unlimited team members, 3 boards, and 300+
+            export-made templates. Signing up with your work email lets you
+            bring in your team faster. See our <br />{" "}
+            <span className="text-[#4262FF]">pricing plans</span> for more
+            features.
+          </p>
         </div>
         <div className=" px-12 text-left py-5">
-            <h1 className="font-bold text-primary text-2xl">Easy integrations</h1>
-            <p className="text-base mt-5">Miro has 100+ powerful integrations with tools you already use like G Suite, Slack, and Jira, so your workflow is seamless. View the full list in our <span className="text-[#4262FF]">Marketplace.</span> </p>
+          <h1 className="font-bold text-primary text-2xl">Easy integrations</h1>
+          <p className="text-base mt-5">
+            Miro has 100+ powerful integrations with tools you already use like
+            G Suite, Slack, and Jira, so your workflow is seamless. View the
+            full list in our{" "}
+            <span className="text-[#4262FF]">Marketplace.</span>{" "}
+          </p>
         </div>
         <div className=" px-12 text-left py-5">
-            <h1 className="font-bold text-primary text-2xl">Security first</h1>
-            <p className="text-base mt-5">We treat your data like you would - with the utmost care. We fellow industry-leading security standards and give you tools to protect intellectual property. Learn more to our <span className="text-[#4262FF]">Trust Center</span>.</p>
+          <h1 className="font-bold text-primary text-2xl">Security first</h1>
+          <p className="text-base mt-5">
+            We treat your data like you would - with the utmost care. We fellow
+            industry-leading security standards and give you tools to protect
+            intellectual property. Learn more to our{" "}
+            <span className="text-[#4262FF]">Trust Center</span>.
+          </p>
         </div>
       </div>
       <div className="items-center flex justify-center my-5">
-      <button 
-      className="rounded-2xl bg-secondary text-white px-3 
-      py-2 flex items-center gap-1 justify-center">
-        Sign up free <FaArrowRight />
-      </button>
+        <button
+          className="rounded-2xl bg-secondary text-white px-3 
+      py-2 flex items-center gap-1 justify-center"
+        >
+          Sign up free <FaArrowRight />
+        </button>
       </div>
-      
     </div>
   );
 };
